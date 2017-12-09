5952eb1451cb1ae3ab5ea953b57dcbb9b28d20c1
philip ka sing leung <philip.leung90@gmail.com>
2017-12-09 14:14:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,6 +20,8 @@ function addElementToEndOfArray(array , element){
 
 function destructivelyAddElementToEndOfArray (array, element){
   
-  return array.push(element);
+  array.push(element);
+  
+  return array;
   
 }
